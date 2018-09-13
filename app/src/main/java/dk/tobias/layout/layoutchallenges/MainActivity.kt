a4b6744662baf530ba19c3faadb32c35146f41bc
Tobias Valbjørn <tvalbjoern@gmail.com>
2018-09-13 21:26:31 +0200

    1st challange now working. Next is implementing challenge two with the radio button

--- app/src/main/java/dk/tobias/layout/layoutchallenges/MainActivity.kt
+++ app/src/main/java/dk/tobias/layout/layoutchallenges/MainActivity.kt
@@ -3,23 +3,44 @@ package dk.tobias.layout.layoutchallenges
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 
+
+fun sortString(inputStr:String): String{
+
+    val arr=inputStr.toCharArray()
+    arr.sort()
+    val result: String = "["+arr.joinToString("")+"]"
+    return result
+}
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get reference to button
+        // get references
         val btnclick= findViewById<Button>(R.id.button)
         val changedoutput= findViewById<TextView>(R.id.output)
-
+        val input= findViewById<EditText>(R.id.editText)
         // set on-click listener
         btnclick.setOnClickListener{
-            changedoutput.text = "hej"
+            if (input != null) {
+                val str=input.text.toString()
+                if(str.isNotEmpty()){
+                changedoutput.text = sortString(str)}
+            }
+
+
+
+
+
         }
     }
 
